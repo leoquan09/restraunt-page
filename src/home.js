d322ec186983e877e4695929278015d4cf7c845f
Leo <leoquan09@gmail.com>
2026-01-05 19:21:42 -0800

    ig im done

--- src/home.js
+++ src/home.js
@@ -1,13 +1,12 @@
 let content = 
-    `
-    
+`
+<div class="item">
     <h1>Leo's linguinis</h1>
     <p>
     At Leo's linguinis, we believe in the best and source all ingredients from only the finest farms with organic non-gmo vegetables blah blah blah blah blah blah blah blah blah blah blah blah
     blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
     </p>
-    
-    
+</div> 
     `;
 
 export default content
--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,14 @@
 let content = 
 `
-<div class="menuItems">
+<div class="item">
     <h1>Butter Linguini</h1>
     <p>
     This is our signature dish and one of our two. It comes with lots of linguini and a side of garlic bread for extra happiness.
     It's made by our world renowned chefs and is also the gateway to infinite diabetes and obesety. You are only allowed to order one per sitting.
     </p>
 </div>
-<div class="menuItems">
-    <h1>fun Linguini</h1>
+<div class="item">
+    <h1>Diabetes in a jar</h1>
     <p>
     This is our other signature dish and one of our two. It comes with lots of linguini and a side of garlic bread for extra happiness.
     It's made by our world renowned chefs and is also the gateway to infinite diabetes and obesety. You are only allowed to order one per sitting.
